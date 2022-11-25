7b3ef0f24dfbc5dfff86b8b003861720c760729f
양지윤 <jiyuunyang@pcanpi.com>
2022-11-25 16:32:25 +0900

    [Add] Add component test

--- components/HeaderFixed.tsx
+++ components/HeaderFixed.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 export const Component = styled.header`
   box-shadow: 2px 4px 8px 0 rgba(0, 0, 0, 0.04), 0 2px 6px 0 rgba(0, 0, 0, 0.04), 1px 1px 4px 0 rgba(0, 0, 0, 0.12);
   position: fixed;
+  font-size: 30px;
+  color: black;
   left: 0;
   right: 0;
   top: 0;
