6e86b390785349ea5bf0bb4fbe43542c23db172d
양지윤 <jiyuunyang@pcanpi.com>
2022-11-25 14:46:12 +0900

    [Add] Add header components

---
+++ components/HeaderSticky.tsx
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Component = styled.header`
+  box-shadow: 2px 4px 8px 0 rgba(0, 0, 0, 0.04), 0 2px 6px 0 rgba(0, 0, 0, 0.04), 1px 1px 4px 0 rgba(0, 0, 0, 0.12);
+  position: sticky;
+  left: 0;
+  right: 0;
+  top: 0;
+  width: 100%;
+  z-index: 100;
+  background-color: yellow;
+`;
+
+export const HeaderSticky = () => {
+  return (<Component>
+    This is sticky header
+  </Component>)
+}
