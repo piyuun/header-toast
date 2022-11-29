ced5e104cf154aeaa852f9ec86a863e7560fc42b
양지윤 <jiyuunyang@pcanpi.com>
2022-11-29 13:11:49 +0900

    [Test] Fix toast position: top-center

--- components/WordLimitToast.tsx
+++ components/WordLimitToast.tsx
@@ -25,7 +25,7 @@ const WordLimitToast = () => {
 
 export const handleWordLimitToast = () => {
   return toast(({}) => <WordLimitToast />, {
-    position: 'bottom-center',
+    position: 'top-center',
     autoClose: 2000,
     hideProgressBar: true,
     closeOnClick: true,
